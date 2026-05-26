b04cc82e3873431e1fc020a44de870c5c4894446
Manuel Romero <23680177@uagro.mx>
2026-05-26 02:41:14 -0600

    Update Carrera.js

--- models/Carrera.js
+++ models/Carrera.js
@@ -3,5 +3,5 @@ const carreraSchema = new mongoose.Schema({
     nombre: String,
     facultad: String,
     activo: { type: Boolean, default: true }
-}, { collection: 'Carreras' });
+});
 module.exports = mongoose.model('Carrera', carreraSchema);
